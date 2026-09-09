9af5283e89d9918b93f5f9bcb0e717a86e60970e
Nolhan Dolou <o22403372@or-iut-i007-f21>
2026-09-09 16:54:01 +0200

    fin heure td, fin td1 p9

--- monProj/monApp/tests.py
+++ monProj/monApp/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+class HomeViewTest(TestCase):
+
+    # test de l'url de la page d'accueil
+    def test_home_status_code(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    # test du contenu de la page d'accueil
+    def test_home_content(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, 'Hello Django!')
