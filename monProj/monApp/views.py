f2934306af9a68f86df6de2f98db07e8547d0a99
Nolhan Dolou <o22403372@or-iut-i202-f09>
2026-09-10 09:47:23 +0200

    ajout contact aboutus views

--- monProj/monApp/views.py
+++ monProj/monApp/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
     return HttpResponse("<h1>Hello Django!</h1>")
+
+
+def contact(request):
+    return HttpResponse("<h1>Bienvenue sur la page de contact</h1>")
+
+def aboutus(request):
+    return HttpResponse("<h1>Bienvenue sur la page d'infomations</h1>")
