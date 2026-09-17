76b9278cdb7f79dd0dfa23ae78ca1efc06944576
Nolhan Dolou <o22403372@or-iut-i202-f09>
2026-09-17 09:47:37 +0200

    ajout dossier tests + nouveau fichier test_view_basics.py

---
+++ monProj/monApp/tests/test_View_Basics.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class HomeViewTest(TestCase):
+
+    # test de l'url de la page d'accueil
+    def test_home_status_code(self):
+                    response = self.client.get(reverse('default'))
+                    self.assertEqual(response.status_code, 200)
+    
+    def test_home_param_status_code(self):
+        response = self.client.get(reverse('home', args=["test"]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact_status_code(self):
+            response = self.client.get(reverse('contact'))
+            self.assertEqual(response.status_code, 200)
+
+    def test_about_status_code(self):
+            response = self.client.get(reverse('aboutus'))
+            self.assertEqual(response.status_code, 200)
+#----------------------------------------------------------------------------------
+    # test du contenu de la page d'accueil
+    def test_home_content(self):
+        response = self.client.get(reverse('default'))
+        self.assertContains(response, 'Hello default!')
+
+    def test_home_param_content(self):
+        response = self.client.get(reverse('home', args=["test"]))
+        self.assertContains(response, 'Hello test!')
+
+    def test_contact_content(self):
+        response = self.client.get(reverse('contact'))
+        self.assertContains(response, '<h1>Bienvenue sur la page de contact</h1>')
+
+    def test_aboutus_content(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertContains(response, "<h1>Bienvenue sur la page d'infomations</h1>")
