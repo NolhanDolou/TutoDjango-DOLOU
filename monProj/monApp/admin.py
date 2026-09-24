46b2b86afc83828eb4eff945229578fb9d895371
NolhanDolou <nolhan.dolou@etu.univ-orleans.fr>
2026-09-24 10:19:05 +0200

    td2 admin tous models + radiobutton p.5

--- monProj/monApp/admin.py
+++ monProj/monApp/admin.py
@@ -4,9 +4,23 @@ from .models import Produit, Categorie, Statut, Rayon
 # Register your models here.
 
 class ProduitAdmin(admin.ModelAdmin):
-    list_display=('intituleProd', 'prixUnitaireProd')
+    model=Produit
+    list_display = ["refProd", "intituleProd", "prixUnitaireProd", "dateFabProd", "categorie", "statut"]
+    list_editable = ["intituleProd", "prixUnitaireProd", "dateFabProd"]
+    radio_fields = {"statut": admin.VERTICAL}
+
+
+
+class ProduitInline(admin.TabularInline):
+    model=Produit
+    extra=1 #nb lignes vides par defaut
+
+class CategorieAdmin(admin.ModelAdmin):
+    model=Categorie
+    inlines=[ProduitInline]
+
 
 admin.site.register(Produit, ProduitAdmin)
-admin.site.register(Categorie)
+admin.site.register(Categorie, CategorieAdmin)
 admin.site.register(Statut)
 admin.site.register(Rayon)
