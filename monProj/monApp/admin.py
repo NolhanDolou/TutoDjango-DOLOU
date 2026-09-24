61f71cb98cd54232c270ae917a5302ebb60a6edf
NolhanDolou <nolhan.dolou@etu.univ-orleans.fr>
2026-09-24 10:30:03 +0200

    fin filtre + hierarchie date

--- monProj/monApp/admin.py
+++ monProj/monApp/admin.py
@@ -3,11 +3,31 @@ from .models import Produit, Categorie, Statut, Rayon
 
 # Register your models here.
 
+class ProduitFilter(admin.SimpleListFilter):
+    title='filtre produit'
+    parameter_name='custom_statut'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('OnLine', 'En ligne'),
+            ('OffLine', 'Hors ligne'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'OnLine':
+            return queryset.filter(statut=1)
+        if self.value() == 'OffLine':
+            return queryset.filter(statut=0)
+
+
 class ProduitAdmin(admin.ModelAdmin):
     model=Produit
     list_display = ["refProd", "intituleProd", "prixUnitaireProd", "dateFabProd", "categorie", "statut"]
     list_editable = ["intituleProd", "prixUnitaireProd", "dateFabProd"]
     radio_fields = {"statut": admin.VERTICAL}
+    search_fields = ('intituleProd', 'dateFabProd')
+    list_filter = (ProduitFilter,)
+    date_hierarchy = 'dateFabProd'
 
 
 
