7ef21a9995845c4ee97a5e8b02e7efcd26e1b5f0
NolhanDolou <nolhan.dolou@etu.univ-orleans.fr>
2026-09-24 10:13:22 +0200

    fin tests tp1

--- monProj/monApp/admin.py
+++ monProj/monApp/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Produit
+from .models import Produit, Categorie, Statut, Rayon
 
 # Register your models here.
 
 class ProduitAdmin(admin.ModelAdmin):
     list_display=('intituleProd', 'prixUnitaireProd')
+
 admin.site.register(Produit, ProduitAdmin)
+admin.site.register(Categorie)
+admin.site.register(Statut)
+admin.site.register(Rayon)
--- monProj/monApp/tests/test_View_Lists.py
+++ monProj/monApp/tests/test_View_Lists.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from monApp.models import Produit, Categorie, Rayon, Statut
 
 class ListViewTest(TestCase):
 
@@ -7,7 +8,12 @@ class ListViewTest(TestCase):
         self.categorie = Categorie.objects.create(nomCat="Boissons")
         self.rayon = Rayon.objects.create(nomRayon="Rayon A")
         self.statut = Statut.objects.create(libelleStatut="Disponible")
-        self.produit = Produit.objects.create(intituleProd="Coca-Cola")
+        self.produit = Produit.objects.create(
+            intituleProd="Coca-Cola",
+            prixUnitaireProd=1.50,
+            categorie=self.categorie,
+            statut=self.statut,
+        )
 
 
     # test de l'url de la page d'accueil
@@ -29,3 +35,19 @@ class ListViewTest(TestCase):
 
 
 #----------------------------------------------------------------------------------
+
+    def test_liste_produits_content(self):
+        response = self.client.get(reverse('ListeProduits'))
+        self.assertContains(response, f"<li>{self.produit.intituleProd}</li>")
+
+    def test_liste_categories_content(self):
+        response = self.client.get(reverse('ListeCategories'))
+        self.assertContains(response, f"<li>{self.categorie.nomCat}</li>")
+
+    def test_liste_rayons_content(self):
+        response = self.client.get(reverse('ListeRayons'))
+        self.assertContains(response, f"<li>{self.rayon.nomRayon}</li>")
+
+    def test_liste_statuts_content(self):
+        response = self.client.get(reverse('ListeStatuts'))
+        self.assertContains(response, f"<li>{self.statut.libelleStatut}</li>")
